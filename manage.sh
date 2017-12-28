cf1644334f0396a79dc303fc216db19a98307b23
Juho Vuori <juho.vuori@zalando.fi>
2017-12-28 14:43:53 +0200

    deploy app

--- manage.sh
+++ manage.sh
@@ -4,23 +4,29 @@ REGION=eu-west-3
 PROFILE=juhocli
 AWS="/usr/local/bin/aws --profile=$PROFILE --region=$REGION"
 INFRA_TPL="$(cat cfn/infrastructure.yaml)"
-APP_TPL="$(cat cfn/app.yaml)"
+APP_VERSION="$(git rev-parse --short HEAD)"
+APP_PARAMS="ParameterKey=Version,ParameterValue=$APP_VERSION"
 TARGET_DIR="$(pwd)/target"
-ARTIFACT_BUCKET="notes-artifacts-20170909"
-ARTIFACT_PREFIX="lambda/"
+ARTIFACT_BUCKET="notes-artifacts-20171228"
+ARTIFACT_LAMBDA_PREFIX="lambda/"
+ARTIFACT_CFN_PREFIX="cfn/"
+APP_TPL="https://s3.eu-west-3.amazonaws.com/$ARTIFACT_BUCKET/${ARTIFACT_CFN_PREFIX}app-$APP_VERSION.yaml"
 
 mkdir -p "$TARGET_DIR"
 
 create() {
 	STACK_NAME=$1; shift
-	TPL=$2; shift
-	$AWS cloudformation create-stack --stack-name "$STACK_NAME" --template-body "$TPL"
+	TPL=$1; shift
+	PARAMS=$1; shift
+	echo  		$AWS cloudformation create-stack --stack-name "$STACK_NAME" --parameters="$PARAMS" --template-url "$TPL"
+	$AWS cloudformation create-stack --capabilities CAPABILITY_IAM --stack-name "$STACK_NAME" --parameters="$PARAMS" --template-url "$TPL"
 }
 
 update() {
 	STACK_NAME=$1; shift
-	TPL=$2; shift
-	$AWS cloudformation update-stack --stack-name "$STACK_NAME" --template-body "$TPL"
+	TPL=$1; shift
+	PARAMS=$1; shift
+	$AWS cloudformation update-stack --stack-name "$STACK_NAME" --parameters="$PARAMS" --template-url "$TPL"
 }
 
 delete() {
@@ -49,7 +55,15 @@ push_app() {
 	VERSION=$1; shift
 	ZIP_FILE="$TARGET_DIR/app-$VERSION.zip"
 	create_zip_file $ZIP_FILE
-	$AWS s3 cp "$ZIP_FILE" "s3://$ARTIFACT_BUCKET/${ARTIFACT_PREFIX}app-$VERSION.zip"
+	$AWS s3 cp "$ZIP_FILE" "s3://$ARTIFACT_BUCKET/${ARTIFACT_LAMBDA_PREFIX}app-$VERSION.zip"
+}
+
+push_cfn() {
+	TPL_BASE=$1; shift
+	VERSION=$1; shift
+	SRC="cfn/$TPL_BASE.yaml"
+	DEST="s3://$ARTIFACT_BUCKET/$ARTIFACT_CFN_PREFIX$TPL_BASE-$VERSION.yaml"
+	$AWS s3 cp "$SRC" "$DEST"
 }
 
 create_lambda() {
@@ -95,17 +109,22 @@ do
 		;;
 		delete-infra) delete notes-infra "$INFRA_TPL"
 		;;
-		create-app) create notes-app "$APP_TPL"
+		create-app) create notes-app "$APP_TPL" "$APP_PARAMS"
 		;;
-		update-app) update notes-app "$APP_TPL"
+		update-app) update notes-app "$APP_TPL" "$APP_PARAMS"
 		;;
-		delete-app) delete notes-app "$APP_TPL"
+		delete-app) delete notes-app "$APP_TPL" "$APP_PARAMS"
 		;;
 		list) list
 		;;
 		create-l-zip) create_zip_file "$TARGET_DIR/l.zip"
 		;;
-		push-app) push_app $(git rev-parse --short HEAD)
+		push-app-cfn) push_cfn app "$APP_VERSION"
+		;;
+		push-infra-cfn) push_cfn infra "$1"
+		shift
+		;;
+		push-app) push_app "$APP_VERSION"
 		;;
 		create-lambda) create_lambda "$1" "$2"
 		shift 2
